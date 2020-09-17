00a1505661afff8aa0c1cb729d0e47f75ef519bf
Yang Siyu <siyuyang1995@gmail.com>
2020-09-17 18:39:44 +0800

    [yang siyu]feat: get das between next labor day

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +13,14 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int thisYear = date.getYear();
+    LocalDate thisYearLaborDay = LocalDate.of(thisYear, 5, 1);
+    if (date.isAfter(thisYearLaborDay)) {
+      LocalDate nextLaborDay = LocalDate.of(thisYear + 1, 5, 1);
+      long period = nextLaborDay.toEpochDay() - date.toEpochDay();
+      return period;
+    }
+   long period = Period.between(date, thisYearLaborDay).getDays();
+    return period;
   }
 }
